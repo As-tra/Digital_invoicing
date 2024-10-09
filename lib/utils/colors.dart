4c342d94a6db715caaa5d52631e3f60505c5f6b5
Assil <astradhdbjd@gmail.com>
2024-10-09 15:09:06 +0100

    add app styles & colors & fonts

---
+++ lib/utils/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const primaryText = Color(0xff1C1B1F);
+  static const purple = Color(0xff9570F1);
+  static const lightPurple = Color(0xffEFEBFB);
+  static const lightOrange = Color(0xffFDF4EA);
+  static const white = Color(0xffffffff);
+  static const orange = Color(0xffF3A35A);
+  static const yellow = Color(0xffF8DB7A);
+  static const lightText = Color.fromARGB(153, 60, 60, 67);
+}
---
+++ lib/utils/styles.dart
@@ -0,0 +1,56 @@
+import 'package:digital_invoicing/constants.dart';
+import 'package:digital_invoicing/utils/colors.dart';
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static const TextStyle gTWalsheimProRegular10 = TextStyle(
+    fontFamily: kGTWalsheimPro,
+    fontSize: 10,
+    color: AppColors.lightText,
+  );
+  static const TextStyle gTWalsheimProRegular17 = TextStyle(
+    fontFamily: kGTWalsheimPro,
+    fontSize: 17,
+    color: AppColors.white,
+  );
+  static const TextStyle gTWalsheimProRegular9 = TextStyle(
+    fontFamily: kGTWalsheimPro,
+    fontSize: 9,
+    color: AppColors.lightText,
+  );
+  static const TextStyle gTWalsheimProRegular12 = TextStyle(
+    fontFamily: kGTWalsheimPro,
+    fontSize: 12,
+    color: AppColors.lightText,
+  );
+
+  static const TextStyle gTWalsheimProRegular16 = TextStyle(
+    fontFamily: kGTWalsheimPro,
+    fontSize: 16,
+    color: AppColors.lightText,
+  );
+  static const TextStyle gTWalsheimProMedium12 = TextStyle(
+    fontFamily: kGTWalsheimPro,
+    fontSize: 12,
+    color: AppColors.purple,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle gTWalsheimProMedium18 = TextStyle(
+    fontFamily: kGTWalsheimPro,
+    fontSize: 18,
+    color: AppColors.primaryText,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle gTWalsheimProMedium13 = TextStyle(
+    fontFamily: kGTWalsheimPro,
+    fontSize: 13,
+    color: AppColors.primaryText,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle gTWalsheimProBold34 = TextStyle(
+    fontFamily: kGTWalsheimPro,
+    fontSize: 34,
+    color: AppColors.primaryText,
+    fontWeight: FontWeight.w700,
+  );
+}
